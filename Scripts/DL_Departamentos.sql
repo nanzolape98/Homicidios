e842fe6e351da364c9e016d5b44d11d53f0190f4
nanzolape98 <nicolas.anzola80@gmail.com>
2025-10-11 14:46:13 -0500

    Creación de la tabla DL_Departamentos

---
+++ Scripts/DL_Departamentos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE DataLake.dbo.DL_Departamentos (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO varchar(56) NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	[Geo Departamento] varchar(32) NULL
+);
